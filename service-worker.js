f70836d7978feeeac61b983a2b8c9ad4adf4f6a9
mm12346 <xoctell338@gmail.com>
2025-06-07 08:35:28 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'usd-stock-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.html',
+  '/manifest.json',
+  // เพิ่มไฟล์ css, js, icon, ฯลฯ ที่ต้องการ cache
+];
+
+// ติดตั้ง service worker และ cache ไฟล์
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// ดักจับ fetch request
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// อัปเดต service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
